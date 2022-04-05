fd98311e9590937a44bae1479a25f15c1fff2089
TorokDan <dani.torok8@gmail.com>
2022-04-05 13:33:29 +0200

    Néhány elem törölve.

--- GenerikusTarolo/Tarolo.cs
+++ GenerikusTarolo/Tarolo.cs
@@ -2,6 +2,22 @@ namespace GenerikusTarolo
 {
     public class Tarolo
     {
-        
+        private string[] tomb;
+
+        public Tarolo()
+        {
+            tomb = new string[0];
+        }
+
+        public void Hozzaadas(string elem)
+        {
+            string[] tmp = new string[tomb.Length + 1];
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tmp[i] = tomb[i];
+            }
+            tmp[tmp.Length - 1] = elem;
+            tomb = tmp;
+        }
     }
 }
